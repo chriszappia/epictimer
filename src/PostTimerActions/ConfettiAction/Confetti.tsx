284c7a011fbcec692577bffb04b3b2dae537810a
jamesgiu <jjg>
2020-10-13 08:36:47 -0400

    JSDoc + slight performance fix

--- src/PostTimerActions/ConfettiAction/Confetti.tsx
+++ src/PostTimerActions/ConfettiAction/Confetti.tsx
@@ -2,36 +2,44 @@ import React, { useState, useEffect } from 'react';
 import { PostTimerAction } from "../PostTimerAction";
 import Confetti from 'react-confetti';
 
+/**
+ * A PostTimerAction that rains Confetti on the screen while flashing using the EpicConfetti
+ * component.
+ */
 export class ConfettiAction extends PostTimerAction {
-    private timer: NodeJS.Timeout | null;
-
     constructor() {
         super("Confetti");
-        this.timer = null;
     }
 
+    /** @inheritDoc */
     onTimerComplete(): JSX.Element {
         return (
             <EpicConfetti/>
         );
     }
 
+    /** @inheritDoc */
     cleanup(): void {
+        // Do nothing.
     }
 }
 
+/**
+ * The EpicConfetti component that rains confetti on the screen and flashes yellow light.
+ */
 export function EpicConfetti() : JSX.Element {
     const [currentColour, setCurrentColour] = useState<string>('yellow');
 
+    /**
+     * Toggles the current colour in state after a delay of 100ms.
+     * The useEffect React-hook lifecycle function runs on first render and after every state update, which means
+     * that this function will continue to run while the component is mounted.
+     */
     useEffect(() => {
-        const interval = setInterval(() => {
+       setTimeout(() => {
             setCurrentColour(current => current === 'yellow' ? 'grey' : 'yellow');
         }, 100);
-        return () => {
-            // Cleanup
-            clearInterval(interval);
-        };
-    }, []);
+    });
 
     return (
         <div style={{backgroundColor: currentColour, height: "100%"}}>
